d889dc2563439069788073802f1f8b1afee04018
hambrecht <>
2024-07-10 10:27:50 -0700

    refactor: Update dsims.R script
    Update dsims.R script to load shapefile from correct file path, replace numbers with letters in strata name, create survey region with updated strata name and shape, adjust x.space parameter in make.density to 600, and update population description with fixed population size of 200. Improve functionality and accuracy.

--- dsims.R
+++ dsims.R
@@ -1,7 +1,7 @@
 # install packages
 # install.packages("terra")
 # First, ensure you have a copy of the devtools package
-#if (!nzchar(system.file(package = "devtools"))) install.packages("devtools")
+# if (!nzchar(system.file(package = "devtools"))) install.packages("devtools")
 # then ensure you have a copy of the dssd package:
 # if (!nzchar(system.file(package = "dssd"))) devtools::install_github("DistanceDevelopment/dssd", build_vignettes = TRUE)
 # finally install dsims from github:
@@ -15,8 +15,8 @@ library(sf)
 library(lwgeom)
 
 # Define the height and field of view
-altitude <- 100  # height in meters
-fov <- 60      # field of view in degrees
+altitude <- 100 # height in meters
+fov <- 60 # field of view in degrees
 
 # Calculate the width using the tangent function in meters
 image_wdith <- 2 * altitude * tan((fov * (pi / 180)) / 2)
@@ -50,10 +50,12 @@ if (all(all_valid)) {
 
 
 # Create the survey region
-region <- make.region(region.name = "study area",
-                      units = "m",
-                      strata.name = "A",
-                      shape = wum_strata)
+region <- make.region(
+  region.name = "study area",
+  units = "m",
+  strata.name = "A",
+  shape = wum_strata
+)
 
 region@region$geometry <- st_make_valid(region@region$geometry)
 # The plot function allows plotting in km or m.
@@ -62,10 +64,12 @@ plot(region)
 
 
 ## # Create the density surface
-density <- make.density(region = region,
-                        x.space = 600,
-                        constant = 1,
-                        density.surface = dsm.xy)
+density <- make.density(
+  region = region,
+  x.space = 600,
+  constant = 1,
+  density.surface = dsm.xy
+)
 
 
 # Add a hotspot to the density surface, centre located at x = 15000, y = 4000 with
@@ -81,17 +85,21 @@ plot(density, region)
 
 
 # Create the population description, with a population size N = 200
-pop.desc <- make.population.description(region = region,
-                                        density = density ,
-                                        N = rep(200, length(region@strata.name)),
-                                        fixed.N = TRUE)
+pop.desc <- make.population.description(
+  region = region,
+  density = density,
+  N = rep(200, length(region@strata.name)),
+  fixed.N = TRUE
+)
 
 
 # Create the covariate list
 covariate.list <- list()
 # The population will be 50% males and 50% females
-covariate.list$sex <- list(data.frame(level = c("female", "male"),
-                                      prob = c(0.5,0.5)))
+covariate.list$sex <- list(data.frame(
+  level = c("female", "male"),
+  prob = c(0.5, 0.5)
+))
 
 
 ## # Create the population description, with a population size N = 200
@@ -101,10 +109,12 @@ covariate.list$sex <- list(data.frame(level = c("female", "male"),
 ##                                             N = 200)
 
 # Create the population description, with a population size N = 200
-pop.desc.cov <- make.population.description(region = region,
-                                            density = density,
-                                            covariates = covariate.list,
-                                            N = 200)
+pop.desc.cov <- make.population.description(
+  region = region,
+  density = density,
+  covariates = covariate.list,
+  N = 200
+)
 
 covariate.list <- list()
 covariate.list$size <- list(list(distribution = "poisson", lambda = 35))
@@ -112,22 +122,28 @@ covariate.list$size <- list(list(distribution = "poisson", lambda = 35))
 
 
 # Make a simple half normal detection function with a scale parameter of 200
-detect.hn <- make.detectability(key.function = "hn",
-                                 scale.param = 200,
-                                 truncation = image_wdith)
+detect.hn <- make.detectability(
+  key.function = "hn",
+  scale.param = 200,
+  truncation = image_wdith
+)
 # We can now visualise these detection functions
 plot(detect.hn, pop.desc)
 
 # Create the covariate parameter list
 cov.params <- list()
 # Note the covariate parameters are supplied on the log scale
-cov.params$sex = data.frame(level = c("female", "male"),
-                            param = c(0, 1.5))
+cov.params$sex <- data.frame(
+  level = c("female", "male"),
+  param = c(0, 1.5)
+)
 
-detect.cov <- make.detectability(key.function = "hn" ,
-                                 scale.param = 120,
-                                 cov.param = cov.params,
-                                 truncation = 1000)
+detect.cov <- make.detectability(
+  key.function = "hn",
+  scale.param = 120,
+  cov.param = cov.params,
+  truncation = 1000
+)
 
 
 
@@ -137,26 +153,32 @@ plot(detect.cov, pop.desc.cov)
 
 
 # Define the design
-design <- make.design(region = region,
-                      transect.type = "line",
-                      design = "systematic",
-                      spacing = 1000,
-                      truncation = image_wdith)
+design <- make.design(
+  region = region,
+  transect.type = "line",
+  design = "systematic",
+  spacing = 1000,
+  truncation = image_wdith
+)
 
-parallel.design <- make.design(region = region,
-                               design = "systematic",
-                               spacing = 2500,
-                               edge.protocol = "minus",
-                               design.angle = 90,
-                               truncation = image_wdith)
+parallel.design <- make.design(
+  region = region,
+  design = "systematic",
+  spacing = 2500,
+  edge.protocol = "minus",
+  design.angle = 90,
+  truncation = image_wdith
+)
 
-zigzag.design <- make.design(region = region,
-                             design = "eszigzag",
-                             spacing = 1000,
-                             edge.protocol = "minus",
-                             design.angle = 0,
-                             bounding.shape = "convex.hull",
-                             truncation = image_wdith)
+zigzag.design <- make.design(
+  region = region,
+  design = "eszigzag",
+  spacing = 1000,
+  edge.protocol = "minus",
+  design.angle = 0,
+  bounding.shape = "convex.hull",
+  truncation = image_wdith
+)
 
 
 transects <- generate.transects(design)
@@ -167,10 +189,12 @@ plot(region, parallel.transects)
 plot(region, zigzag.transects)
 
 
-ddf.analyses <- make.ds.analysis(dfmodel = list(~1, ~1),
-                                 key = c("hn", "hr"),
-                                 criteria = "AIC",
-                                 truncation = 600)
+ddf.analyses <- make.ds.analysis(
+  dfmodel = list(~1, ~1),
+  key = c("hn", "hr"),
+  criteria = "AIC",
+  truncation = 600
+)
 
 
 
@@ -183,11 +207,13 @@ ddf.analyses <- make.ds.analysis(dfmodel = list(~1, ~1),
 ## # Produce simulation setup plots
 ## check.sim.setup(sim)
 
-sim <- make.simulation(reps = 999,
-                       design = design,
-                       population.description = pop.desc,
-                       detectability = detect.hn,
-                       ds.analysis = ddf.analyses)
+sim <- make.simulation(
+  reps = 999,
+  design = design,
+  population.description = pop.desc,
+  detectability = detect.hn,
+  ds.analysis = ddf.analyses
+)
 # Produce survey and plot it
 survey <- run.survey(sim)
 plot(survey, region)
@@ -197,6 +223,3 @@ sim <- run.simulation(simulation = sim, run.parallel = F)
 summary(sim)
 # Display a histogram of the estimates of abundance
 histogram.N.ests(sim)
-
-
-

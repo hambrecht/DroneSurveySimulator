f9a5faf53acf9aa33a38ba1d669011d1dcbbfad4
hambrecht <>
2024-07-10 10:27:33 -0700

    refactor: Update obs2density_surface.R script
    This commit updates the obs2density_surface.R script by adding necessary libraries (terra, Distance, sf, dplyr) and removing unnecessary code. It also renames columns, converts units to km, and creates separate dataframes for segmentation, distance, and observation data. Additionally, it creates a prediction grid based on the WMU outline shapefile and performs exploratory data analysis, estimation of detection function, fitting of a DSM, autocorrelation analysis, and abundance estimation.

--- obs2density_surface.R
+++ obs2density_surface.R
@@ -8,21 +8,23 @@ library(sf)
 library(dplyr)
 
 # plotting options
-gg.opts <- theme(panel.grid.major=element_blank(),
-                panel.grid.minor=element_blank(),
-                panel.background=element_blank())
+gg.opts <- theme(
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  panel.background = element_blank()
+)
 
 # make the results reproducible
 set.seed(11123)
 
-# load data 
-data <- sf::st_read('D:\\WMU\\survey_data\\501_moose_locations.shp')
+# load data
+data <- sf::st_read("D:\\WMU\\survey_data\\501_moose_locations.shp")
 head(data)
-plot(data,  max.plot = 1)
+plot(data, max.plot = 1)
 
 # rename columns
 data$Transect.Label <- data$name
-data$Sample.Label <- paste(data$name, data$ident, sep = '_')
+data$Sample.Label <- paste(data$name, data$ident, sep = "_")
 data$object <- data$ident
 data$size <- 1
 
@@ -31,18 +33,18 @@ data$Effort <- 10
 data$distance <- data$distance / 1000
 
 # remove unnessecary colums
-data <- data[ , -which(names(data) %in% c("distance_2", "aircraft", "date", "y_proj", "x_proj", "name", 'layer', "ident"))]
+data <- data[, -which(names(data) %in% c("distance_2", "aircraft", "date", "y_proj", "x_proj", "name", "layer", "ident"))]
 
 # check data
 head(data)
 
 # create seperate df
-segdata <- sf::st_drop_geometry(data[ , which(names(data) %in% c("Latitude", "Longitude", "Effort", "Transect.Label", "Sample.Label"))])
-distdata <- sf::st_drop_geometry(data[ , which(names(data) %in% c("object", "Latitude", "Longitude", "distance", "Effort", "size"))])
+segdata <- sf::st_drop_geometry(data[, which(names(data) %in% c("Latitude", "Longitude", "Effort", "Transect.Label", "Sample.Label"))])
+distdata <- sf::st_drop_geometry(data[, which(names(data) %in% c("object", "Latitude", "Longitude", "distance", "Effort", "size"))])
 distdata$detected <- 1
-segdata$x <- distdata$x <- sf::st_coordinates(data)[,1]
-segdata$y <- distdata$y <- sf::st_coordinates(data)[,2]
-obsdata <- sf::st_drop_geometry(data[ , which(names(data) %in% c("object", "distance", "Effort", "Sample.Label", "size"))])
+segdata$x <- distdata$x <- sf::st_coordinates(data)[, 1]
+segdata$y <- distdata$y <- sf::st_coordinates(data)[, 2]
+obsdata <- sf::st_drop_geometry(data[, which(names(data) %in% c("object", "distance", "Effort", "Sample.Label", "size"))])
 
 head(segdata)
 head(distdata)
@@ -56,7 +58,7 @@ head(obsdata)
 wmu <- sf::st_read("D:\\WMU\\base_data\\WMU\\wmu_501_3400.shp")
 
 # Create an empty SpatRaster
-grid <- rast(ext(wmu), resolution=500) # Use ext instead of extent
+grid <- rast(ext(wmu), resolution = 500) # Use ext instead of extent
 crs(grid) <- crs(wmu) # Ensure the correct object (wmu) is referenced
 gridpolygon <- as.polygons(grid)
 wmu_vect <- vect(wmu)
@@ -66,17 +68,16 @@ plot(pred_grid)
 # given the argument fill (the covariate vector to use as the fill) and a name,
 # return a geom_polygon object
 # fill must be in the same order as the polygon data
-grid_plot_obj <- function(fill, name, sf_obj){
-  
+grid_plot_obj <- function(fill, name, sf_obj) {
   # Prepare the data
   data <- data.frame(fill = fill)
   names(data) <- name
-  
+
   # Combine data with geometry from the sf object
-  combined_sf <- sf_obj %>% 
-    mutate(id = row_number()) %>% 
+  combined_sf <- sf_obj %>%
+    mutate(id = row_number()) %>%
     left_join(data, by = c("id" = "row.names"))
-  
+
   # Plot using ggplot2 and the combined sf object
   ggplot(combined_sf) +
     geom_sf(aes_string(fill = name)) +
@@ -90,7 +91,7 @@ centroids_sf <- st_as_sf(centroids, coords = c("x", "y"), crs = 3400, agr = "con
 areas <- terra::expanse(pred_grid)
 
 # Combine coordinates and areas into a data frame
-preddata <- data.frame(x = sf::st_coordinates(centroids_sf)[,1], y = sf::st_coordinates(centroids_sf)[,2], area = areas)
+preddata <- data.frame(x = sf::st_coordinates(centroids_sf)[, 1], y = sf::st_coordinates(centroids_sf)[, 2], area = areas)
 
 # Display the first few rows of the data frame
 head(preddata)
@@ -100,29 +101,32 @@ head(preddata)
 # Explorartory data anaylsis
 # Distance data
 # histograms
-hist(distdata$distance,main="",xlab="Distance (m)")
+hist(distdata$distance, main = "", xlab = "Distance (m)")
 
 # Estimating the detection function
-detfc.hr.null<-ds(distdata, max(distdata$distance), key="hr", adjustment=NULL)
+detfc.hr.null <- ds(distdata, max(distdata$distance), key = "hr", adjustment = NULL)
 summary(detfc.hr.null)
-par(mfrow=c(1,2))
-plot(detfc.hr.null, showpoints=FALSE, pl.den=0, lwd=2)
+par(mfrow = c(1, 2))
+plot(detfc.hr.null, showpoints = FALSE, pl.den = 0, lwd = 2)
 ddf.gof(detfc.hr.null$ddf)
 
 # Fitting a DSM
-dsm.xy <- dsm(count~s(x,y), detfc.hr.null, segdata, obsdata, method="REML")
+dsm.xy <- dsm(count ~ s(x, y), detfc.hr.null, segdata, obsdata, method = "REML")
 summary(dsm.xy)
-vis.gam(dsm.xy, plot.type="contour", view=c("x","y"), asp=1, type="response", contour.col="black", n.grid=100)
-plot(data,  max.plot = 1, add = TRUE)
+vis.gam(dsm.xy, plot.type = "contour", view = c("x", "y"), asp = 1, type = "response", contour.col = "black", n.grid = 100)
+plot(data, max.plot = 1, add = TRUE)
 gam.check(dsm.xy)
 rqgam_check(dsm.xy)
 
 # Autocorrelation
-dsm_cor(dsm.xy, max.lag=10, Segment.Label="Sample.Label")
+dsm_cor(dsm.xy, max.lag = 10, Segment.Label = "Sample.Label")
 
 # Abundance estimation
 dsm.xy.pred <- predict(dsm.xy, preddata, preddata$area)
-p <- ggplot() + grid_plot_obj(dsm.xy.pred, "Abundance", pred_grid) + coord_equal() +gg.opts
-p <- p + geom_path(aes(x=x, y=y),data=survey.area)
-p <- p + labs(fill="Abundance")
+p <- ggplot() +
+  grid_plot_obj(dsm.xy.pred, "Abundance", pred_grid) +
+  coord_equal() +
+  gg.opts
+p <- p + geom_path(aes(x = x, y = y), data = survey.area)
+p <- p + labs(fill = "Abundance")
 print(p)

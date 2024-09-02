aa9a674ac52085ba1e2f76035b71dbcfe262e70e
hambrecht <137965368+hambrecht@users.noreply.github.com>
2024-09-02 09:10:08 -0700

    Replace `.` with `_`

--- Scripts/simulation.R
+++ Scripts/simulation.R
@@ -44,7 +44,7 @@ ALTITUDE <- 700          # Height in meters
 IMAGE_WIDTH <- calculate_image_width(ALTITUDE)
 
 # Create population description
-pop.desc <- make.population.description(
+pop_desc <- make.population.description(
   region = region,
   density = density,
   N = total_abundance,
@@ -52,12 +52,20 @@ pop.desc <- make.population.description(
 )
 
 # Define and visualise detection function
-detect.hn <- make.detectability(
+detect_hn <- make.detectability(
   key.function = "hn",
   scale.param = 200,
   truncation = IMAGE_WIDTH
 )
-plot(detect.hn, pop.desc)
+plot(detect_hn, pop_desc)
+
+# Define and visualise uniform detection function
+detect_uf <- make.detectability(
+  key.function = "uf",
+  scale.param = 1,
+  truncation = IMAGE_WIDTH
+)
+plot(detect_uf, pop_desc)
 
 # Define survey design
 design <- make.design(
@@ -82,7 +90,7 @@ transects <- generate.transects(design)
 plot(region, transects)
 
 # Define analysis models
-ddf.analyses <- make.ds.analysis(
+ddf_analyses <- make.ds.analysis(
   dfmodel = list(~1, ~1),
   key = c("hn", "hr"),
   criteria = "AIC",
@@ -93,18 +101,16 @@ ddf.analyses <- make.ds.analysis(
 sim <- make.simulation(
   reps = 99,
   design = design,
-  population.description = pop.desc,
-  detectability = detect.hn,
-  ds.analysis = ddf.analyses
+  population.description = pop_desc,
+  detectability = detect_hn,
+  ds.analysis = ddf_analyses
 )
 
 survey <- run.survey(sim)
 plot(survey, region)
-plot(survey@population)
-hist(survey@dist.data$distance)
 
 # Run the full simulation
-sim <- run.simulation(simulation = sim, run.parallel = TRUE)
+sim <- run.simulation(simulation = sim, run.parallel = T)
 
 # Display results
 summary(sim)
@@ -117,51 +123,51 @@ truncation_distances <- c(
   calculate_image_width(500)
 )
 
-results.list <- vector("list", length(truncation_distances))
-summary.list <- vector("list", length(truncation_distances))
+results_list <- vector("list", length(truncation_distances))
+summary_list <- vector("list", length(truncation_distances))
 
 for (i in seq_along(truncation_distances)) {
   cat(sprintf("\nRunning for truncation = %d", truncation_distances[i]))
   
-  new.ds.analyses <- make.ds.analysis(
+  new_ds_analyses <- make.ds.analysis(
     dfmodel = list(~1, ~1),
     key = c("hn", "hr"),
     criteria = "AIC",
     truncation = truncation_distances[i]
   )
   
-  sim@ds.analysis <- new.ds.analyses
-  results.list[[i]] <- run.simulation(sim, run.parallel = TRUE)
-  summary.list[[i]] <- summary(results.list[[i]], description.summary = FALSE)
+  sim@ds.analysis <- new_ds_analyses
+  results_list[[i]] <- run.simulation(sim, run.parallel = F)
+  summary_list[[i]] <- summary(results_list[[i]], description.summary = FALSE)
 }
 
-names(results.list) <- paste0("t", truncation_distances)
-names(summary.list) <- paste0("t", truncation_distances)
+names(results_list) <- paste0("t", truncation_distances)
+names(summary_list) <- paste0("t", truncation_distances)
 
 
 # Extracting results statistics
 
-N    <- unlist(lapply(summary.list, function(x){x@individuals$N$mean.Estimate}))
-n    <- unlist(lapply(summary.list, function(x){x@individuals$summary$mean.n}))
-se   <- unlist(lapply(summary.list, function(x){x@individuals$N$mean.se}))
-sd.N <- unlist(lapply(summary.list, function(x){x@individuals$N$sd.of.means}))
-bias <- unlist(lapply(summary.list, function(x){x@individuals$N$percent.bias}))
-RMSE <- unlist(lapply(summary.list, function(x){x@individuals$N$RMSE}))
-cov  <- unlist(lapply(summary.list, function(x){x@individuals$N$CI.coverage.prob}))
+N    <- unlist(lapply(summary_list, function(x){x@individuals$N$mean.Estimate}))
+n    <- unlist(lapply(summary_list, function(x){x@individuals$summary$mean.n}))
+se   <- unlist(lapply(summary_list, function(x){x@individuals$N$mean.se}))
+sd_N <- unlist(lapply(summary_list, function(x){x@individuals$N$sd.of.means}))
+bias <- unlist(lapply(summary_list, function(x){x@individuals$N$percent.bias}))
+RMSE <- unlist(lapply(summary_list, function(x){x@individuals$N$RMSE}))
+cov  <- unlist(lapply(summary_list, function(x){x@individuals$N$CI.coverage.prob}))
 
-sim.data <- data.frame(trunc = truncation_distances, 
+sim_data <- data.frame(trunc = truncation_distances,
                        n = round(n),
                        N = round(N),
                        se = round(se,2),
-                       sd.N = round(sd.N,2),
+                       sd.N = round(sd_N,2),
                        bias = round(bias,2),
                        RMSE = round(RMSE,2),
                        cov = round(cov*100,1))
 
-kable(sim.data, 
+kable(sim_data,
       col.names = c("$Truncation$", "$mean\\ n$", "$mean\\ \\hat{N}$", "$mean\\ se$", "$SD(\\hat{N})$", "$\\% Bias$", "$RMSE$", "$\\%\\ CI\\ Coverage$"),
       row.names = FALSE,
       align = c('c', 'c', 'c', 'c', 'c', 'c', 'c', 'c'),
       caption = "Simulation Results for the simple half normal detection probability: The truncation distance, mean number of detections, mean estimated population size (N), mean standard error of $\\hat{N}$, the standard deviation of $\\hat{N}$, percentage bias, root mean squared error, percentage of times the true value of N was captured in the confidence intervals.",
       table.placement="!h",
-      format = "html")
+      format = "simple")

b27575104c239b348167964867e00b558d7c6e27
Leonard Hambrecht <137965368+hambrecht@users.noreply.github.com>
2024-09-24 14:23:20 -0700

    Create a loop to work through all wmu and save the detection functions, model selection and dsm into a file

--- Scripts/density_analysis.R
+++ Scripts/density_analysis.R
@@ -44,8 +44,8 @@ possDS <- possibly(.f = Distance::ds, otherwise = NULL)
 # Define grid cell size (in meters)
 GRID_SIZE <- 500
 wmu_number_list <- c('501', '503', '512', '517', '528')
-wmu_number <- wmu_number_list[3]
-# for (wmu_number in wmu_number_list){
+# wmu_number <- wmu_number_list[1]
+for (wmu_number in wmu_number_list){
 
   ## Load and Check Data
 
@@ -88,46 +88,22 @@ wmu_number <- wmu_number_list[3]
   # Extract coordinates
   coords <- sf::st_drop_geometry(density@density.surface[[1]][, c("x", "y")])
 
-
-  # Plot distance data
-par(mfrow = c(1, 1))
-  break_bins <- seq(from = 0, to = 0.6, by = 0.05)
-  hist(distdata$distance, main = "Moose line transects", xlab = "Distance (km)", breaks = break_bins)
-  quantile(distdata$distance, 0.995)
-  max(distdata$distance)
-
-
+  ## Distance Sampling Models
   truncation_distance_list <- c(round(max(distdata$distance),1), round(quantile(distdata$distance, 0.995),2))
   # detfc_list <- list()
   detfc_list_bin <- list()
 
+  # Fixed bin size
+  bin_size <- 0.1
 
   # Loop through truncation distances
   for (truncation_distance in truncation_distance_list) {
-    # Create detection function models
-    # detfc_list[[paste0("detfc_hr_null_trunc_", truncation_distance)]] <- possDS(distdata, truncation_distance, key = "hr", adjustment = NULL)
-    # detfc_list[[paste0("detfc_hn_null_trunc_", truncation_distance)]] <- possDS(distdata, truncation_distance, key = "hn", adjustment = NULL)
-    # detfc_list[[paste0("detfc_hn_cos_trunc_", truncation_distance)]] <- possDS(distdata, truncation_distance, key = "hn", adjustment = "cos")
-    print(paste0("Truncation distance: ", truncation_distance,"m"))
-    # Loop through bin sizes
-    for (i in seq(from = 0.1, to = 0.1, by = 0.01)) {
-      # Create detection function models with bins
-      print(paste0("Bin size: ", i,"m"))
-      detfc_list_bin[[paste0("detfc_hr_null_trunc_", truncation_distance, "_bin_", i)]] <- possDS(distdata, truncation_distance, key = "hr", adjustment = NULL, cutpoints = seq(from = 0, to = truncation_distance, by = i))
-      detfc_list_bin[[paste0("detfc_hn_null_trunc_", truncation_distance, "_bin_", i)]] <- possDS(distdata, truncation_distance, key = "hn", adjustment = NULL, cutpoints = seq(from = 0, to = truncation_distance, by = i))
-      detfc_list_bin[[paste0("detfc_hn_cos_trunc_", truncation_distance, "_bin_", i)]] <- possDS(distdata, truncation_distance, key = "hn", adjustment = "cos", max_adjustments = 2,  monotonicity = 'strict', cutpoints = seq(from = 0, to = truncation_distance, by = i))
-    }
-  }
-
-  # Loop through truncation distances
-  for (truncation_distance in truncation_distance_list) {
-    # Loop through bin sizes
-    for (i in seq(from = 0.1, to = 0.1, by = 0.01)) {
-      # Create detection function models with bins
-      # detfc_list_bin[[paste0("detfc_cc_hr_null_trunc_", truncation_distance, "_bin_", i)]] <- possDS(distdata, truncation_distance, formula = ~as.factor(canopy_cover), key = "hr", adjustment = NULL, cutpoints = seq(from = 0, to = truncation_distance, by = i))
-      detfc_list_bin[[paste0("detfc_cc_hn_null_trunc_", truncation_distance, "_bin_", i)]] <- possDS(distdata, truncation_distance, formula = ~as.factor(canopy_cover), key = "hn", adjustment = NULL, cutpoints = seq(from = 0, to = truncation_distance, by = i))
-      detfc_list_bin[[paste0("detfc_cc_hn_cos_trunc_", truncation_distance, "_bin_", i)]] <- possDS(distdata, truncation_distance, formula = ~as.factor(canopy_cover), key = "hn", adjustment = "cos", max_adjustments = 2,  monotonicity = 'strict', cutpoints = seq(from = 0, to = truncation_distance, by = i))
-    }
+      detfc_list_bin[[paste0("detfc_hr_null_trunc_", truncation_distance, "_bin_", bin_size)]] <- possDS(distdata, truncation_distance, key = "hr", adjustment = NULL, cutpoints = seq(from = 0, to = truncation_distance, by = bin_size))
+      detfc_list_bin[[paste0("detfc_hn_null_trunc_", truncation_distance, "_bin_", bin_size)]] <- possDS(distdata, truncation_distance, key = "hn", adjustment = NULL, cutpoints = seq(from = 0, to = truncation_distance, by = bin_size))
+      detfc_list_bin[[paste0("detfc_hn_cos_trunc_", truncation_distance, "_bin_", bin_size)]] <- possDS(distdata, truncation_distance, key = "hn", adjustment = "cos", max_adjustments = 2,  monotonicity = 'strict', cutpoints = seq(from = 0, to = truncation_distance, by = bin_size))
+      detfc_list_bin[[paste0("detfc_cc_hr_null_trunc_", truncation_distance, "_bin_", bin_size)]] <- possDS(distdata, truncation_distance, formula = ~as.factor(canopy_cover), key = "hr", adjustment = NULL, cutpoints = seq(from = 0, to = truncation_distance, by = bin_size))
+      detfc_list_bin[[paste0("detfc_cc_hn_null_trunc_", truncation_distance, "_bin_", bin_size)]] <- possDS(distdata, truncation_distance, formula = ~as.factor(canopy_cover), key = "hn", adjustment = NULL, cutpoints = seq(from = 0, to = truncation_distance, by = bin_size))
+      detfc_list_bin[[paste0("detfc_cc_hn_cos_trunc_", truncation_distance, "_bin_", bin_size)]] <- possDS(distdata, truncation_distance, formula = ~as.factor(canopy_cover), key = "hn", adjustment = "cos", max_adjustments = 2,  monotonicity = 'strict', cutpoints = seq(from = 0, to = truncation_distance, by = bin_size))
   }
 
 
@@ -136,76 +112,66 @@ par(mfrow = c(1, 1))
   # detfc_list_compact <- compact(detfc_list)
   detfc_list_bin_compact <- compact(detfc_list_bin)
 
-output_path <- here("Output", "DSM", paste0("detfc",wmu_number,".RData"))
-save(detfc_list_bin_compact, file = output_path)
+# output_path <- here("Output", "DSM", paste0("detfc",wmu_number,".RData"))
+# save(detfc_list_bin_compact, file = output_path)
+# input_path <- here("Output", "DSM", paste0("detfc",wmu_number,".RData"))
+# load(input_path)
 
   aic_values_bin <- purrr::map_dbl(detfc_list_bin_compact, ~ .x$ddf$criterion)
-# Extract CV values for each model
-cv_values_bin <- purrr::map_dbl(detfc_list_bin_compact, function(model) {
-  model_summary <- summary(model)
-  cv_value <- model_summary$ds$Nhat.se[1] / model_summary$ds$Nhat
-  return(cv_value)
-})
+  # Extract CV values for each model
+  cv_values_bin <- purrr::map_dbl(detfc_list_bin_compact, function(model) {
+    model_summary <- summary(model)
+    cv_value <- model_summary$ds$Nhat.se[1] / model_summary$ds$Nhat
+    return(cv_value)
+  })
 
-# Extract Chi-Square values for each model
-chi_square_values_bin <- purrr::map_dbl(detfc_list_bin_compact, function(model) {
-  gof_test <- ddf.gof(model$ddf)
-  chi_square_value <- gof_test$chisquare$chi1$chisq
-  return(chi_square_value)
-})
+  # Extract Chi-Square values for each model
+  chi_square_values_bin <- purrr::map_dbl(detfc_list_bin_compact, function(model) {
+    gof_test <- ddf.gof(model$ddf)
+    chi_square_value <- gof_test$chisquare$chi1$chisq
+    return(chi_square_value)
+  })
 
-# Combine AIC and CV values into a dataframe
-model_metrics <- data.frame(
-  Model = names(detfc_list_bin_compact),
-  AIC = aic_values_bin,
-  CV = cv_values_bin,
-  Chi_Square = chi_square_values_bin
-)
-
-# Normalise AIC and CV (assuming lower values are better)
-model_metrics <- model_metrics %>%
-  mutate(
-    AIC_normalised = (AIC - min(AIC)) / (max(AIC) - min(AIC)),
-    CV_normalised = (CV - min(CV)) / (max(CV) - min(CV)),
-    Chi_Square_normalised = (Chi_Square - min(Chi_Square)) / (max(Chi_Square) - min(Chi_Square))
+  # Combine AIC and CV values into a dataframe
+  model_metrics <- data.frame(
+    Model = names(detfc_list_bin_compact),
+    AIC = aic_values_bin,
+    CV = cv_values_bin,
+    Chi_Square = chi_square_values_bin
   )
 
-# Define weights for AIC, CV and Chi-Square (adjust these weights according to your preference)
-weight_AIC <- 1
-weight_CV <- 1
-weight_Chi_Square <- 1
+  # Normalise AIC and CV (assuming lower values are better)
+  model_metrics <- model_metrics %>%
+    mutate(
+      AIC_normalised = (AIC - min(AIC)) / (max(AIC) - min(AIC)),
+      CV_normalised = (CV - min(CV)) / (max(CV) - min(CV)),
+      Chi_Square_normalised = (Chi_Square - min(Chi_Square)) / (max(Chi_Square) - min(Chi_Square))
+  )
 
-# Combine the normalized values using the weights
-model_metrics <- model_metrics %>%
-  mutate(
-    combined_score = weight_AIC * AIC_normalised + weight_CV * CV_normalised + weight_Chi_Square * Chi_Square_normalised
-  ) %>%
-  arrange(combined_score)
+  # Define weights for AIC, CV and Chi-Square (adjust these weights according to your preference)
+  weight_AIC <- 1
+  weight_CV <- 1
+  weight_Chi_Square <- 1
 
-row.names(model_metrics) <- NULL
+  # Combine the normalized values using the weights
+  model_metrics <- model_metrics %>%
+    mutate(
+      combined_score = weight_AIC * AIC_normalised + weight_CV * CV_normalised + weight_Chi_Square * Chi_Square_normalised
+    ) %>%
+    arrange(combined_score)
 
-# Plot the models
-par(mfrow = c(2, 3))
-for (model_name in model_metrics$Model) {
-  model <- detfc_list_bin[[model_name]]
-  plot(model, showpoints = FALSE, pl.den = 0, lwd = 2, xlim = c(0,0.6), ylim = c(0,1), main = paste(model_name, "AIC:", round(model$ddf$criterion,2)))
-  # ddf.gof(model$ddf, qq = TRUE, main = model_name)
-}
-par(mfrow = c(1, 1))
+  row.names(model_metrics) <- NULL
 
-  best_model <- detfc_list_bin[[names(sorted_aic_diff)[4]]]
-best_summary<- summary(best_summary)
-best_cv <- (best_summary$ds$Nhat.se[1]/best_summary$ds$Nhat)
-  par(mfrow = c(1, 2))
-  plot(best_model, showpoints = FALSE, pl.den = 0, lwd = 2, ylim = c(0,1), main = names(sorted_aic_diff)[1])
-  test <- ddf.gof(best_model$ddf, asp=1, qq = TRUE)
-  test$chisquare$chi1$p
-  qqplot.ddf(best_model$ddf)
-  par(mfrow = c(1, 1))
 
-for (i in 1:length(detfc_list_bin_compact)) {
-  detfc_list_bin_compact[[i]]$name <- names(detfc_list_bin_compact)[i]
-}
+  best_model_list <- list()
+  best_model <- detfc_list_bin_compact[[model_metrics$Model[1]]]
+  best_summary<- summary(best_model)
+  best_cv <- (best_summary$ds$Nhat.se[1]/best_summary$ds$Nhat)
+  best_model_list <- list(model = best_model, summary=best_summary, cv = best_cv)
+
+  for (i in 1:length(detfc_list_bin_compact)) {
+    detfc_list_bin_compact[[i]]$name <- names(detfc_list_bin_compact)[i]
+  }
 
   ## Fit and Analyse Distance Sampling Models
   dsm_list <- list()
@@ -225,6 +191,34 @@ for (i in 1:length(detfc_list_bin_compact)) {
   for (i in 1:length(dsm_list)) {
     dsm_list[[i]]$name <- names(dsm_list)[i]
   }
+
+  # save DSM list
+  output_path <- here("Output", "DSM", paste0("dsm",wmu_number,".RData"))
+  save(dsm_list, detfc_list_bin_compact, best_model_list, file = output_path)
+}
+
+# Plot distance data
+par(mfrow = c(1, 1))
+break_bins <- seq(from = 0, to = 0.6, by = 0.05)
+hist(distdata$distance, main = "Moose line transects", xlab = "Distance (km)", breaks = break_bins)
+quantile(distdata$distance, 0.995)
+max(distdata$distance)
+
+# Plot the models
+par(mfrow = c(2, 3))
+for (model_name in model_metrics$Model) {
+  model <- detfc_list_bin_compact[[model_name]]
+  plot(model, showpoints = FALSE, pl.den = 0, lwd = 2, xlim = c(0,0.6), ylim = c(0,1), main = paste(model_name, "AIC:", round(model$ddf$criterion,2)))
+  # ddf.gof(model$ddf, qq = TRUE, main = model_name)
+}
+par(mfrow = c(1, 1))
+
+par(mfrow = c(1, 2))
+plot(best_model, showpoints = FALSE, pl.den = 0, lwd = 2, ylim = c(0,1), main = names(sorted_aic_diff)[1])
+test <- ddf.gof(best_model$ddf, asp=1, qq = TRUE)
+test$chisquare$chi1$p
+qqplot.ddf(best_model$ddf)
+par(mfrow = c(1, 1))
 
 
   ## Model Checking
@@ -270,15 +264,17 @@ for (i in 1:length(detfc_list_bin_compact)) {
   # load(file = input_path)
   knitr::kable(mod_results, col.names = c("Model name", "Description", "Deviance explained"))
 
-  # save DSM list
-  output_path <- here("Output", "DSM", paste0("dsm",wmu_number,".RData"))
-  save(dsm_list, detfc_list_bin_compact, file = output_path)
-
 
   ## Abundance Estimation
 
   # Predict abundance using the null model
-  dsm_xy_pred <- predict(dsm_list[[1]], coords, dsm_list[[1]]$offset[1])
+  # Find the index of the sublist with name "Second"
+index <- which(sapply(dsm_list, function(x) x$name) == "dsm_xy_detfc_hn_null_trunc_0.43_bin_0.1")
+
+# Access the sublist
+
+  sel_model <- dsm_list[[index]]
+  dsm_xy_pred <- predict(sel_model, coords, sel_model$offset)
 
   # Calculate total abundance over the survey area
   total_abundance <- sum(dsm_xy_pred)
@@ -298,5 +294,5 @@ for (i in 1:length(detfc_list_bin_compact)) {
 
   # Save processed data
   output_path <- here("Output", "Density", paste0("density",wmu_number,".RData"))
-  save(density, total_abundance, abudance_strata_list, region, file = output_path)
-# }
+  save(density, total_abundance, region, file = output_path)
+

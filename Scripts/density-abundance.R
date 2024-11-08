9c5fc753ebc93a32ec3453e79ace33ad00b9b767
Leonard Hambrecht <137965368+hambrecht@users.noreply.github.com>
2024-11-08 13:36:10 -0800

    Enhance abundance estimation by selecting the best model and calculating model variance; update output to include model name and variance in saved data

--- Scripts/density-abundance.R
+++ Scripts/density-abundance.R
@@ -1,10 +1,11 @@
 library(here)
 library(dsims)
+library(dsm) # for dsm_var_gam function
 
 # Define grid cell size (in meters)
 GRID_SIZE <- 500
 wmu_number_list <- c('501', '503', '512', '517', '528')
-wmu_number <- wmu_number_list[1]
+wmu_number <- wmu_number_list[4]
 # save DSM list
 input_path <- here("Output", "DSM", paste0("dsm",wmu_number,".RData"))
 load(file = input_path)
@@ -28,16 +29,21 @@ density <- dsims::make.density(
 coords <- sf::st_drop_geometry(density@density.surface[[1]][, c("x", "y")])
 
 ## Abundance Estimation
-
-# Predict abundance using the null model
-# Find the index of the sublist with name "Second"
-index <- which(sapply(dsm_list, function(x) x$name) == "dsm_xy_detfc_hn_cos_trunc_0.43_bin_0.1")
-
-# Access the sublist
-
-sel_model <- dsm_list[[index]]
+# Select the best model based on the output of vis-detfc-dsm script. Often the model with the lowest AIC value is selected.
+# in this case the model with hazard rate key and no adjustment is selected and 99.5% trunctaion distance is used.
+# Get indices of entries with 'hr_null' in the name
+indices_hr_null <- grep("hr_null", dsm_list)
+# Filter out indices that also have 'trunc_0.6' in the name
+indices_final <- indices_hr_null[!grepl("trunc_0.6|cc", dsm_list[indices_hr_null])]
+model_name <- names(dsm_list[indices_final])
+print(paste0("The model that is use is: ",model_name))
+sel_model <- dsm_list[indices_final][[1]]
+par(mfrow = c(1, 2))
+sel_model <- dsm_list[5][[1]]
+summary(sel_model)
+vis.gam(sel_model, plot.type = "contour", view = c("x", "y"), asp = 1, type = "response", contour.col = "black", n.grid = 500)
 dsm_xy_pred <- predict(sel_model, coords, rep(sel_model$offset[1], length(coords[[1]])))
-dsm_var_gam(sel_model, coords, rep(sel_model$offset[1], length(coords[[1]])))
+model_variance <- dsm_var_gam(sel_model, coords, rep(sel_model$offset[1], length(coords[[1]])))
 
 # Calculate total abundance over the survey area
 total_abundance <- sum(dsm_xy_pred)
@@ -45,11 +51,11 @@ total_abundance <- sum(dsm_xy_pred)
 # Update density object with predicted values
 density@density.surface[[1]]$density <- dsm_xy_pred
 
-# Plot density surface
-plot(density)
-plot(density@density.surface[[1]]['density'])
 
 
 # Save processed data
 output_path <- here("Output", "Density", paste0("density",wmu_number,".RData"))
-save(density, total_abundance, region, file = output_path)
+save(density, total_abundance, region, wmu, model_variance, model_name, file = output_path)
+
+# test
+plot(density)

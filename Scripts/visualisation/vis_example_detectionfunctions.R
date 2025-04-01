cd28dc2ecb020846cb488e2c68bc9267514f96ca
Leonard Hambrecht <137965368+hambrecht@users.noreply.github.com>
2025-04-01 15:33:39 -0700

    Export to TIFF file for publication

--- Scripts/visualisation/vis_example_detectionfunctions.R
+++ Scripts/visualisation/vis_example_detectionfunctions.R
@@ -1,4 +1,9 @@
+# Load necessary library
+library(grDevices)
 library(RColorBrewer)
+library(here)
+
+
 # Function to compute detectability values
 compute_detectability <- function(key.function, scale.param, shape.param, truncation) {
   distances <- seq(0, truncation, length.out = 100)
@@ -22,17 +27,24 @@ detect_uf_data <- compute_detectability("uf", 1, NULL, 50)
 # Define custom colors
 custom_colors <- c("#1f78b4", "#1f78b4", "#a6cee3", "#b2df8a")
 
+# Set the output file path using the here package
+output_path <- here("Output", "Plots", "detectability_plot.tiff")
+
+# Open a TIFF device
+tiff(filename = output_path, width = 2250, height = 1125, units = "px", res = 300)
+
 # Set graphical parameters for A4 size
-par(mar = c(5, 5, 4, 2) + 0.1, cex = 1.2, cex.lab = 1.5, cex.axis = 1.2, lwd = 2, bty = "n")
+par(mar = c(5, 5, 4, 2) + 0.1, cex = 0.9, cex.lab = 0.9, cex.axis = 0.9, lwd = 1, bty = "n", family = "Arial")
 
 # Plot the data
-plot(detect_FW2_data$distance, detect_FW2_data$detectability, type = "l", col = custom_colors[1], lwd = 3, xlim = c(0, 600), ylim = c(0, 1), xlab = "Distance", ylab = "Detectability")
-lines(detect_FWG_data$distance, detect_FWG_data$detectability, col = custom_colors[2], lwd = 3, lty = 2) # Dotted line
-lines(detect_uf_data$distance, detect_uf_data$detectability, col = custom_colors[3], lwd = 3)
-lines(detect_H_data$distance, detect_H_data$detectability, col = custom_colors[4], lwd = 3)
+plot(detect_FW2_data$distance, detect_FW2_data$detectability, type = "l", col = custom_colors[1], lwd = 2, xlim = c(0, 600), ylim = c(0, 1), xlab = "Distance", ylab = "Detectability")
+lines(detect_FWG_data$distance, detect_FWG_data$detectability, col = custom_colors[2], lwd = 2, lty = 2) # Dotted line
+lines(detect_uf_data$distance, detect_uf_data$detectability, col = custom_colors[3], lwd = 2)
+lines(detect_H_data$distance, detect_H_data$detectability, col = custom_colors[4], lwd = 2)
 
 # Add a legend
-legend("topright", legend = c("2 Cameras", "Gimbal", "NADIR", "Helicopter"), col = custom_colors, lty = c(1, 1, 2, 1), lwd = 3, cex = 1.5, box.lwd = 2, box.col = "NA", bg = "white")
+legend("topright", legend = c("2 Cameras", "Gimbal", "NADIR", "Helicopter"), col = custom_colors, lty = c(1, 1, 2, 1), lwd = 2, cex = 0.9, box.lwd = 1, box.col = "NA", bg = "white")
 
-
+# Close the TIFF device
+dev.off()
 
